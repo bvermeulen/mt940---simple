149d54143dcb7d0c3449725603776b350aab60eb
Bruno Vermeulen <bruno_vermeulen2001@yahoo.com>
2018-07-11 20:17:21 +0700

    implemented tkinter editor to parse strings that can not be done automatically

--- mt940m_p36.py
+++ mt940m_p36.py
@@ -1,19 +1,22 @@
 # modules for MT940 ABN-AMRO
 import re
+from mt940e_p36 import Editor
+import pdb
 
 # decode MT940 86 strings and deducts payee and memo
-# for MT940 ABN-AMRO output	
-def code86(string86):
+# for MT940 ABN-AMRO output
+
+def code86(string86, bank_account, date, amount):
     p=''
     m=''
-	
-    # determine the code in line 86 on how to handle it	
+    # determine the code in line 86 on how to handle it
     search_string = re.search('^.{5}',string86)
     code = search_string.group()
 
-    # modify string86 so it will handle '/' in text correctly 
+    # modify string86 so it will handle '/' in text correctly
     string86 = re.sub(r"(/\D{3}/|/\D{4}/)",r"/\1",string86)
-	
+
+    cutoff = 65
     if code == '':
         print ('No code')
 
@@ -30,14 +33,30 @@ def code86(string86):
         # find memo - field after REMI and if this is only a number then add the next field
         string86 = re.sub(r"(/\D{3}/|/\D{4}/)",r"/\1",string86)
         search_string = re.search('//REMI/(.*?(?=//))', string86)
-		
+
         if search_string != None:
             m = search_string.group(1)
 
-        if m.isdigit():		
+        if m.isdigit():
             search_string = re.search('//REMI/.*?(?=/)/(.*?(?=//))', string86)
             if search_string != None:
                 m = m +'/'+search_string.group(1)
+
+    # for SEPA [bank transfer] handle as follows:
+    # find payee account number 'IBAN:', then name 'NAAM:' and description
+    # 'OMSCHRIJVING:'
+    elif code == 'SEPA ':
+        search_string = re.search('IBAN:(.*?(?=BIC:))', string86)
+        if search_string != None:
+            p = search_string.group(1).strip() + ' - '
+            search_string = re.search('NAAM:(.*?(?=OMSCHRIJVING:))', string86)
+            if search_string != None:
+                p = p + search_string.group(1).strip()
+
+        #find memo - field after OMSCHRIJVING:
+        search_string = re.search('OMSCHRIJVING:(.*$)', string86)
+        if search_string != None:
+            m = (search_string.group(1)).strip()
 
     # for BEA [use of card pin code] handle as follows:
     # find payee and set memo
@@ -47,28 +66,24 @@ def code86(string86):
         if search_string != None:
             m = search_string.group(1)
             p = p + re.search(r'\d\d.\d\d\.\d\d/\d\d.\d\d\s(.*?(?=,))', m).group(1)
-	
+
     # for GEA [use of ATM] handle as follows:
     # find ATM machine as payee and set memo
     elif code == 'GEA  ':
         p = 'ATM: '
-        search_string = re.search(r'(\d\d.\d\d\.\d\d/\d\d.\d\d\s.*$)', string86)		
-        if search_string != None:		
+        search_string = re.search(r'(\d\d.\d\d\.\d\d/\d\d.\d\d\s.*$)', string86)
+        if search_string != None:
             m = search_string.group(1)
             p = p + re.search(r'\d\d.\d\d\.\d\d/\d\d.\d\d\s(.*?(?=,))', m).group(1)
-		
-    # where code is not defined just take whole 86 string (stripped from any whitespaces) 
+
+    # where code is not defined just take whole 86 string (stripped from any whitespaces)
     # as memo and set payee to ABN-AMRO
     else:
-        p = 'ABN-AMRO'
-        m = string86
+        string86 = re.sub(' +', ' ', string86)
+        p, m = Editor.edit(string86, bank_account, date, amount)
+        cutoff = 200
 
-
-    m = re.sub('\s+',' ',m.strip())
-    # add space in front as otherwise some transactions are not imported
-    # not clear why this is. For consistancy we do the same for payee
-    # m = ' ' + m
-    # p = ' ' + p
+    m = re.sub('\s+',' ',m.strip())[:cutoff]  # allow m only to be 65 characters long
     return p,m
 
 # convert amount string in proper format and output as string
@@ -78,7 +93,7 @@ def conv_amount_str(creditsign, amount_str):
         sign = '-'
     else:
         sign = ''
-     
+
     amount = '{0}{1}'.format(sign, amount)
     if amount.endswith('.'):
         amount = amount +'00'
@@ -95,7 +110,7 @@ def transaction_date_conversion(v_d,t_d):
     # check month to see valuta date and transaction date are in the same year
     if int(v_d[2:4]) > int(t_d[0:2]):
         year = year+1
-	
+
     # convert to <dd/mm/yyyy> format
     date = t_d[2:4]+'/'+t_d[0:2]+'/'+str(year)
 
@@ -104,14 +119,12 @@ def transaction_date_conversion(v_d,t_d):
 def write_qif_record(qf, date, amount, payee, memo):
 # write the date, amount, payee and memo in qif format:
 #   D<date>
-#   U<amount>
 #   T<amount>
 #   P<payee>
 #   M<memo>
 #   ^
 #
     qf.write('D%s\n' % date)
-    qf.write('U%s\n' % amount)
     qf.write('T%s\n' % amount)
     qf.write('P%s\n' % payee)
     qf.write('M%s\n' % memo)
